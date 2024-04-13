df63d1244e613ff151bd03a84f98f865615eec04
Vaibhav-888 <vaibhav.lohar87@gmail.com>
2024-04-14 01:00:37 +0530

    I have imported the random as 'from random import choice, randint, shuffle' for minizing the number of lines of code in main.py and wiped the 'random' used everytime in order to access the library in main.py file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-import random
+from random import choice, randint, shuffle
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
            'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
@@ -9,20 +9,25 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')']
 
-nr_letters = random.randint(8, 10)
-nr_numbers = random.randint(2, 4)
-nr_symbols = random.randint(2, 4)
+# nr_letters = random.randint(8, 10)
+# nr_numbers = random.randint(2, 4)
+# nr_symbols = random.randint(2, 4)
 
-password_list = []
+# Note: these three lines of code of list comprehensions are replaced by the below three for loops
+password_letters = [choice(letters) for _ in range(randint(8, 10))]
+password_numbers = [choice(numbers) for _ in range(randint(2, 4))]
+password_symbols = [choice(symbols) for _ in range(randint(2, 4))]
 
-for char in range(nr_letters):
-    password_list.append(random.choice(letters))
+password_list = password_letters + password_numbers + password_symbols
 
-for char in range(nr_numbers):
-    password_list.append(random.choice(numbers))
-
-for char in range(nr_symbols):
-    password_list.append(random.choice(symbols))
+# for char in range(nr_letters):
+#     password_list.append(random.choice(letters))
+#
+# for char in range(nr_numbers):
+#     password_list.append(random.choice(numbers))
+#
+# for char in range(nr_symbols):
+#     password_list.append(random.choice(symbols))
 
 random.shuffle(password_list)
 
